2c303c0e8b1920812516fb6355e066d118c5c33e
Benjamin Lavon <benlavon@gmail.com>
2020-03-11 18:53:02 +0000

    3.3.1

--- chapter3/ticket.rb
+++ chapter3/ticket.rb
@@ -9,6 +9,12 @@ class Ticket
   def date
     @date
   end
+  def price=(amount)
+    @price = amount
+  end
+  def price
+    @price
+  end
   def discount(percent)
     @price = @price * (100 - percent) / 100.0
   end
@@ -17,8 +23,8 @@ class Ticket
   end
 end
 
-th = Ticket.new("Town Hall", "2013-11-12")
-cc = Ticket.new("Convention Center", "2014-12-13")
-puts "We've created two tickets."
-puts "The first is for a #{th.venue} event on #{th.date}."
-puts "The second is for an event on #{cc.date} at #{cc.venue}."
+ticket = Ticket.new("Town Hall", "2013-11-12")
+ticket.price=(63.00)
+puts "The ticket costs $#{"%.2f" % ticket.price}."
+ticket.price=(72.50)
+puts "Whoops -- it just went up. It now costs $#{"%.2f" % ticket.price}."
