3b53df46e7f90b366be8abe486c344766c098ced
Benjamin Lavon <benlavon@gmail.com>
2020-03-11 18:48:25 +0000

    3.2.1

--- chapter3/ticket.rb
+++ chapter3/ticket.rb
@@ -1,8 +1,24 @@
 class Ticket
+  def initialize(venue,date)
+    @venue = venue
+    @date = date
+  end
+  def venue
+    @venue
+  end
+  def date
+    @date
+  end
+  def discount(percent)
+    @price = @price * (100 - percent) / 100.0
+  end
   def event
     "Can't really be specified yet..."
   end
 end
 
-ticket = Ticket.new
-puts ticket.event
+th = Ticket.new("Town Hall", "2013-11-12")
+cc = Ticket.new("Convention Center", "2014-12-13")
+puts "We've created two tickets."
+puts "The first is for a #{th.venue} event on #{th.date}."
+puts "The second is for an event on #{cc.date} at #{cc.venue}."
