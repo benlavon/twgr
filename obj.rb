ed8c249c52690cb8fa8a71d93f9952b509fa219c
Benjamin Lavon <benlavon@gmail.com>
2020-03-11 14:42:11 +0000

    2.4.2

--- obj.rb
+++ obj.rb
@@ -32,3 +32,38 @@ if obj.respond_to?("talk")
 else
   puts "Sorry, the object doesn't understand the 'talk' message."
 end
+
+def obj.multi_args(*x)
+  puts "I can take zero or more arguments!"
+end
+
+def two_or_more(a,b,*c)
+  puts "I require two or more arguments!"
+  puts "And sure enough, I got: "
+  p a, b, c
+end
+
+two_or_more(1,2,3,4,5)
+
+def default_args(a,b,c=1)
+  puts "Values of variables: ",a,b,c
+end
+
+default_args(3,2)
+default_args(4,5,6)
+
+def mixed_args(a,b,*c,d)
+  puts "Arguments:"
+  p a,b,c,d
+end
+
+mixed_args(1,2,3,4,5)
+mixed_args(1,2,3)
+
+def args_unleashed(a,b=1,*c,d,e)
+  p a,b,c,d,e
+end
+
+args_unleashed(1,2,3,4,5)
+args_unleashed(1,2,3,4)
+args_unleashed(1,2,3)
