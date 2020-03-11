03297023f083a4af59a0884aa6a298e336826e18
Benjamin Lavon <benlavon@gmail.com>
2020-03-11 14:27:12 +0000

    2.3.1

--- obj.rb
+++ obj.rb
@@ -17,3 +17,12 @@ puts obj.c2f(100)
 def welcome_the_user()
   puts "Hi there!"
 end
+
+puts "The id of the obj is #{obj.object_id}."
+str = "String are objects too, and this is a string!"
+puts "The id of the string object str is #{str.object_id}"
+puts "And the id of the integer 100 is #{100.object_id}"
+
+a = Object.new
+b = a
+puts "a's id is #{a.object_id} and b's id is #{b.object_id}"
