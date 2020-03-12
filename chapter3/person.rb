87eb227afa3359c0edbf0ea4c765d65cd2ff7be0
Benjamin Lavon <benlavon@gmail.com>
2020-03-12 11:25:58 +0000

    3.5

--- chapter3/person.rb
+++ chapter3/person.rb
@@ -7,7 +7,17 @@ class Person
     puts "Returning the person's name..."
     @name
   end
+  def species
+    "Homo sapiens"
+  end
 end
+
 joe = Person.new
 joe.set_name("Joe")
 puts joe.get_name
+
+class Rubyist < Person
+end
+
+david = Rubyist.new
+puts david.species
