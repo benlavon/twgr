7a64b03e32ee285d32b1915e58bfd3f4ccd81cbf
Benjamin Lavon <benlavon@gmail.com>
2020-03-15 23:49:57 +0000

    6.1.3

---
+++ chapter6/quit_or_not.rb
@@ -0,0 +1,16 @@
+def quit_or_not
+  print "Exit the program? (yes or no): "
+  answer = gets.chomp
+  case answer
+  when "yes", "y"
+    puts "Good-bye!"
+    exit
+  when "no", "n"
+    puts "OK, we'll continue"
+  else
+    puts "That's an unknown answer -- assuming you meant 'no'"
+  end
+  puts "Continuing with program...."
+  quit_or_not
+end
+quit_or_not
