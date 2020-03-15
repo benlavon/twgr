b945afbb0d647bbef4653d36f9a6f97f02a09c33
Benjamin Lavon <benlavon@gmail.com>
2020-03-15 21:27:11 +0000

    5.2.5

---
+++ chapter5/car_instance_variables.rb
@@ -0,0 +1,37 @@
+class Car
+  @@makes = []
+  @@cars = {}
+  attr_reader :make
+  def self.total_count
+    @total_count ||= 0
+  end
+  def self.total_count=(n)
+    @total_count = n
+  end
+  def self.add_make(make)
+    unless @@makes.include?(make)
+      @@makes << make
+      @@cars[make] = 0
+    end
+  end
+  def initialize(make)
+    if @@makes.include?(make)
+      puts "Creating a new #{make}!"
+      @make = make
+      @@cars[make] += 1
+      self.class.total_count += 1
+    else
+      raise "No such make: #{make}."
+    end
+  end
+  def make_mates
+    @@cars[self.make]
+  end
+end
+Car.add_make("Honda")
+Car.add_make("Ford")
+class Hybrid < Car
+end
+h3 = Hybrid.new("Honda")
+f2 = Hybrid.new("Ford")
+puts "There are #{Hybrid.total_count} hybrids on the road!"
