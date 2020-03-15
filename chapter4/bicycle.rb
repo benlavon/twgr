5b302a2441c6d260b22d36a3ada133b594eede6c
Benjamin Lavon <benlavon@gmail.com>
2020-03-15 14:55:24 +0000

    4.2.7

--- chapter4/bicycle.rb
+++ chapter4/bicycle.rb
@@ -5,10 +5,22 @@ class Bicycle
     @seats = 1
     @gears = gears
   end
+  def rent
+    puts "Sorry but this model is sold out."
+  end
 end
 class Tandem < Bicycle
-  def initialize
+  def initialize(gears)
     super
     @seats = 2
   end
+  def rent
+    puts "This bike is available!"
+  end
 end
+
+t = Tandem.new(1)
+puts t.method(:rent)
+puts t.method(:rent).super_method
+t.method(:rent).call
+t.method(:rent).super_method.call
