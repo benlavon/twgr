afe607b9be82fd4de0939743ac49ee12a9276727
Benjamin Lavon <benlavon@gmail.com>
2020-03-16 01:26:02 +0000

    6.3.8

---
+++ chapter6/temperature.rb
@@ -0,0 +1,23 @@
+class Temperature
+  def Temperature.c2f(celsius)
+    celsius * 9.0 / 5 + 32
+  end
+  def Temperature.now
+    rand(0..100)
+  end
+end
+celsius = [0, 10, 20, 30, 40, 50, 60, 70, 90, 100]
+fahrenheit = Temperature.now
+puts "The temperature is now: #{fahrenheit} degrees Fahrenheit."
+puts "Celsius\tFahrenheit"
+celsius.each do |c|
+  fahrenheit = Temperature.c2f(c)
+  puts "#{c}\t#{fahrenheit}"
+end
+puts fahrenheit
+
+# Block-local variables
+celsius.each do |c;fahrenheit|
+  fahrenheit = Temperature.c2f(2)
+  puts "#{c}\t#{fahrenheit}"
+end
