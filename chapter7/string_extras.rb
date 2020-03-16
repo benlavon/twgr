366bbca6054523c9b03d6f9a12296f376c3dc318
Benjamin Lavon <benlavon@gmail.com>
2020-03-16 19:44:07 +0000

    7.7.1

---
+++ chapter7/string_extras.rb
@@ -0,0 +1,10 @@
+str = "Another plain old string."
+module StringExtras
+  def shout
+    self.upcase + "!!!"
+  end
+end
+class String
+  include StringExtras
+end
+puts str.methods.include?(:shout)
