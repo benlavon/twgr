e3c6af3636ff3e4f578cafef27452e5e8505f0cf
Benjamin Lavon <benlavon@gmail.com>
2020-03-16 00:09:32 +0000

    6.1.3

--- chapter6/ticket.rb
+++ chapter6/ticket.rb
@@ -1,22 +1,31 @@
 class Ticket
-  attr_accessor :venue, :date
-  def initialize(venue, date)
+  attr_accessor :name, :venue, :date
+  def initialize(name, venue, date)
+    self.name = name
     self.venue = venue
     self.date = date
   end
   def ===(other_ticket)
-    self.venue == other_ticket.venue
+    self.date == other_ticket.date
   end
 end
-ticket1 = Ticket.new("Town Hall", "07/08/18")
-ticket2 = Ticket.new("Conference Center", "07/08/18")
-ticket3 = Ticket.new("Town Hall", "08/09/18")
-puts "ticket1 is for an event at: #{ticket1.venue}"
-case ticket1
-when ticket2
-  puts "Same location as ticket2!"
-when ticket3
-  puts "Same location as ticket3!"
-else
-  puts "No match!"
+
+class TicketComparer
+  def self.compare(ticket1, ticket2, ticket3)
+    puts "#{ticket1.name} is for an event on: #{ticket1.date}"
+    case ticket1
+    when ticket2
+      puts "Same date as ticket2!"
+    when ticket3
+      puts "Same date as ticket3!"
+    else
+      puts "No match!"
+    end
+  end
 end
+
+ticket1 = Ticket.new("ticket1", "Town Hall", "07/08/18")
+ticket2 = Ticket.new("ticket2", "Conference Center", "07/08/18")
+ticket3 = Ticket.new("ticket3", "Town Hall", "08/09/18")
+
+TicketComparer.compare(ticket1, ticket2, ticket3)
